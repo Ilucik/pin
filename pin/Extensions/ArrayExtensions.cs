ad2d5224e15b49469859af7ce04f9970514bcc3d
ilucik <super14142list.ru>
2025-02-16 23:32:39 +0500

    small refactoring

---
+++ pin/Extensions/ArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿
+namespace pin.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static int IndexOfMin(this int[] src)
+        {
+            var min = int.MaxValue;
+            var ind = -1;
+            for (var i = 0; i < src.Length; i++)
+            {
+                if (src[i] < min)
+                {
+                    min = src[i];
+                    ind = i;
+                }
+            }
+
+            return ind;
+        }
+    }
+}
