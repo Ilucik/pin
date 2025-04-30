a45b0e12695cbaa0ad41e9f73186a09ce7e48a8b
ilucik <62407205+Ilucik@users.noreply.github.com>
2025-04-30 17:37:35 +0500

    settings save

--- pin/Infrastructure/IWritableOptions.cs
+++ pin/Infrastructure/IWritableOptions.cs
@@ -5,6 +5,6 @@ namespace pin.Infrastructure
 {
     public interface IWritableOptions<T> : IOptions<T> where T : class, new()
     {
-        void Update(Action<T> applyChanges);
+        public void Update(Action<T> applyChanges);
     }
 }
